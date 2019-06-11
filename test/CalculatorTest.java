9305dcd6d72206fee249a6ae595e34923cc181fd
KhanhNguyen206 <nguyen.ngockhanh206@gmail.com>
2019-06-11 11:55:00 +0700

    RefactorVar-Const

---
+++ test/CalculatorTest.java
@@ -0,0 +1,9 @@
+
+class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator.calculate( 2,3, '+');
+        Calculator.calculate(10, 5, '-');
+        Calculator.calculate(7,3, '*');
+        Calculator.calculate(30,5,'/');
+    }
+}
